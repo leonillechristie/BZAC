7308b5ebeec572653899a744fceef6e207f23bd9
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-06-26 19:46:15 +0800

    added new footer and header

---
+++ header2.php
@@ -0,0 +1,17 @@
+<section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="index.php">Dashboard</a></li>
+                            <li><a class="menu-top-active" href="account.php">Account</a></li>
+                            <li><a href="login.php">Logout</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
