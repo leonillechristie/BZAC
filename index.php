125c5f55cfcca93e0951c4b7affaa067da490ddf
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-06-26 18:17:53 +0800

    added files from original repo

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Free Responsive Admin Theme - ZONTAL</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICONS  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a class="menu-top-active" href="index.php">Dashboard</a></li>
+                            <li><a href="ui.html">UI Elements</a></li>
+                            <li><a href="table.html">Data Tables</a></li>
+                            <li><a href="forms.html">Forms</a></li>
+                             <li><a href="login.php">Login</a></li>
+                            <li><a href="blank.html">Account</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>   
+
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container" style="height:140px;">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                    <img style="margin-top: -20px; margin-left: 40px;" src="assets/img/logo.png" />
+                </a>
+                
+                <a href="index.php">
+                    <h3 style="padding-left: 200px; color: white; position: absolute; padding-top: 20px;">Blessed Zion Academy Cebu <br>Admin Portal</h3>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="page-head-line">Student List</h4>
+
+                </div>
+            </div>
+        
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Student ID</th>
+                            <th>Last Name</th>
+                            <th>First Name</th>
+                            <th>Middle Name</th>
+                            <th>Grade Level</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       
+                        <tr>
+                            <td>18-0038</td>
+                            <td>Alemania</td>
+                            <td>Ziv Samuel</td>
+                            <td>Gumela</td>
+                            <td>Kinder 2</td>
+                            <td class="text-center"> 
+                                <a href="#"  class="btn btn-xs btn-danger">View</a> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>18-0038</td>
+                            <td>Alemania</td>
+                            <td>Ziv Samuel</td>
+                            <td>Gumela</td>
+                            <td>Kinder 2</td>
+                            <td class="text-center"> 
+                                <a href="#"  class="btn btn-xs btn-danger">View</a> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>18-0038</td>
+                            <td>Alemania</td>
+                            <td>Ziv Samuel</td>
+                            <td>Gumela</td>
+                            <td>Kinder 2</td>
+                            <td class="text-center"> 
+                                <a href="#"  class="btn btn-xs btn-danger">View</a> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>18-0038</td>
+                            <td>Alemania</td>
+                            <td>Ziv Samuel</td>
+                            <td>Gumela</td>
+                            <td>Kinder 2</td>
+                            <td class="text-center"> 
+                                <a href="#"  class="btn btn-xs btn-danger">View</a> 
+                            </td>
+                        </tr>
+                            
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2018 BZAC | Web Portal</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+    <script src="assets/js/jquery-1.11.1.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="BZAC Admin Website" />
+    <meta name="author" content="Leonille Christie Lavador" />
+    
+    <title>BZAC - Admin Portal</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICONS  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+    
+   <!--  <section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="index.php">Dashboard</a></li>
+                            <li><a href="ui.html">UI Elements</a></li>
+                            <li><a href="table.html">Data Tables</a></li>
+                            <li><a href="forms.html">Forms</a></li>
+                            <li><a class="menu-top-active" href="login.php">Login</a></li>
+                            <li><a href="blank.html">Account</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section> -->
+
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container" style="height: 140px;">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img style="margin-top: -20px; margin-left: 40px;" src="assets/img/logo.png" />
+                </a>
+                
+                <a href="index.php">
+                    <h3 style="padding-left: 200px; color: white; position: absolute; padding-top: 20px;">Blessed Zion Academy Cebu <br>Admin Portal</h3>
+                </a>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="page-head-line">Login Page </h4>
+
+                </div>
+
+            </div>
+            <div class="row" style="padding-left: 400px;">
+                <div class="col-md-6">
+                     <label>Enter Username </label>
+                        <input type="text" class="form-control" />
+                        <label>Enter Password</label>
+                        <input type="password" class="form-control" />
+                        <div style="margin-top: 15px;margin-left: 270px;">
+                        <a href="index.php" class="btn btn-danger"><span class="glyphicon glyphicon-user"></span> &nbsp;Log In </a>&nbsp;
+                    </div>
+                </div>
+                <hr>
+            </div>
+            
+            <div class="text-center">
+                <h5>Don't have an account? <a href="register.php">Sign Up</a> here.</h5>
+            </div>
+        </div>
+    </div>
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2018 BZAC | Web Portal</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.11.1.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+</body>
+
+</html>
